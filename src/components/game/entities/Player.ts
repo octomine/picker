f90dcb4f1a928320b87a8d996eca4254facb50a3
octo <v.minaev@qiwi.com>
2024-03-23 14:54:36 +0300

    some player refactor

--- src/components/game/entities/Player.ts
+++ src/components/game/entities/Player.ts
@@ -1,8 +1,6 @@
 import { BASE_DRAG, BASE_VELOCITY } from "../constants"
 
 class Player extends Phaser.GameObjects.Sprite {
-    private currentVelocity = BASE_VELOCITY
-
     constructor(scene: Phaser.Scene, x: number, y: number) {
         super(scene, x, y, 'penta')
 
@@ -19,6 +17,10 @@ class Player extends Phaser.GameObjects.Sprite {
 
     get cBody() {
         return this.body as Phaser.Physics.Arcade.Body
+    }
+
+    get currentVelocity() {
+        return BASE_VELOCITY
     }
 }
 
